33c6b1b2d470cdd96bcb16723477bcf918e889ae
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2020-07-29 22:06:41 -0500

    Create MainActivity.kt

---
+++ app/src/main/java/com/my/first/application/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.my.first.application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
